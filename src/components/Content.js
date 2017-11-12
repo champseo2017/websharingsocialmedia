f129ea6a6399c44a7680961b02efb248e6543e33
champseo2017 <boomgt4444@gmail.com>
2017-11-12 12:53:08 +0700

    good

--- src/components/Content.js
+++ src/components/Content.js
@@ -27,34 +27,12 @@ export default class Content extends Component {
         
        
             <p>
-            <a class=""><MyComponent /></a >
-            <a class="btn btn-primary social-login-btn social-twitter" href="/auth/twitter"><i class="fa fa-twitter"></i></a>
-            </p>
-            <p>
-            <a class="btn btn-primary social-login-btn social-linkedin" href="/auth/linkedin"><i class="fa fa-linkedin"></i></a>
+            <MyComponent />
+            
             <Googlelogin />
          </p>
-         <div class="btn-group social-login-more">
-  <button type="button" class="btn btn-default dropdown-toggle btn-block" data-toggle="dropdown">
-    More...
-  </button>
-  <ul class="dropdown-menu text-left " role="menu">
-    
-    <li><a href="#"><i class="fa fa-github-alt"></i>   Github</a></li>
-    <li><a href="#"><i class="fa fa-dropbox"></i>   Dropbox</a></li>
-    <li><a href="/auth/amazon"><span class="zocial-amazon"></span>   Amazon</a></li>
-    <li><a href="#"><span class="zocial-bitbucket"></span>   Bitbucket</a></li>
-    <li><a href="#"><span class="zocial-evernote"></span>   Evernote</a></li>
-    <li><a href="#"><span class="zocial-meetup"></span>   Meetup</a></li>
-    <li><a href="#"><i class="fa fa-windows"></i>   Windows Live</a></li>
-    <li><a href="#"><i class="fa fa-weibo"></i>   Weibo</a></li>
-    <li><a href="#"><i class="fa fa-foursquare"></i>   Foursquare</a></li>
-    
-    <li><a href="#"><i class="fa fa-trello"></i>   Trello</a></li>
-    <li><a href="#"><span class="zocial-wordpress"></span>   Wordpress</a></li>
-  </ul>
-</div>
-<br/><br/>
+        
+
 <small class="text-muted">Or sign in with</small>
 		    <br/><br/>
         <FormGroup>
