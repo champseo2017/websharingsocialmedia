38ba745faf2dae85334b15485457b922be2962ed
champseo2017 <boomgt4444@gmail.com>
2017-11-12 11:30:06 +0700

    ggggggg

--- src/components/Googlelogin.js
+++ src/components/Googlelogin.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from 'react';
 import GoogleLogin from 'react-google-login';
-
-
-class Googlelogin extends React.Component {
-   responseGoogle = (response) => {
-  console.log(response);
-}
-
-  render() {
+ 
+export default class Googlelogin extends Component {
+ 
+  constructor (props, context) {
+    super(props, context);
+  }
+ 
+  responseGoogle (googleUser) {
+    var id_token = googleUser.getAuthResponse().id_token;
+    console.log({accessToken: id_token});
+    //anything else you want to do(save to localStorage)...
+  }
+ 
+  render () {
     return (
-      
-      <GoogleLogin 
-       clientId="254425343264-33g017f9faga6e2g20el5e6ebnu5dc67.apps.googleusercontent.com"
-       buttonText="Login"
-      
-       
-        
-      />
-    )
+      <div>
+        <GoogleLogin socialId="254425343264-33g017f9faga6e2g20el5e6ebnu5dc67.apps.googleusercontent.com"
+                     className="google-login"
+                     scope="profile"
+                     fetchBasicProfile={false}
+                     responseHandler={this.responseGoogle}
+                     buttonText="Login With Google"/>
+      </div>
+    );
   }
 }
-
-export default Googlelogin;
